a823c2ef956f6a59724d684ba7519fb763604fde
Pranav Dhote <166908644+pranavtdhote@users.noreply.github.com>
2026-04-21 00:34:47 +0530

    Refactor binary search to use command line arguments
    Updated binary search implementation to read key from command line arguments and adjusted array size accordingly.

--- Process/child.c
+++ Process/child.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Binary Search
 int binarySearch(int arr[], int n, int key) {
     int low = 0, high = n - 1;
 
     while(low <= high) {
-        int mid = (low + high)/2;
+        int mid = (low + high) / 2;
 
         if(arr[mid] == key)
             return mid;
@@ -14,19 +15,27 @@ int binarySearch(int arr[], int n, int key) {
         else
             high = mid - 1;
     }
+
     return -1;
 }
 
 int main(int argc, char *argv[]) {
-    int n = argc - 1;
+    int n = argc - 2;  // last argument is key
     int arr[n];
 
-    for(int i=0;i<n;i++)
-        arr[i] = atoi(argv[i+1]);
+    // Convert arguments to integers
+    for(int i = 0; i < n; i++) {
+        arr[i] = atoi(argv[i + 1]);
+    }
 
-    int key;
-    printf("Enter element to search: ");
-    scanf("%d", &key);
+    int key = atoi(argv[argc - 1]);
+
+    printf("\nSorted array received: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\nSearching for: %d\n", key);
 
     int result = binarySearch(arr, n, key);
 
